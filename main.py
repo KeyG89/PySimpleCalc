90d8ad0394a1b6dc4f1f10d697211b498d9ef6c6
K. Goscinski <krzysztof.goscinski@gmail.com>
2017-09-25 19:01:02 +0200

    first lines of code

---
+++ main.py
@@ -0,0 +1,18 @@
+import re
+
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    equation = input("Enter equation: ")
+    if equation == 'quit':
+        run = False
+    else:
+        print("You typed: ", equation)
+
+while run:
+    performMath()
